1c8b9342b7d27a7b304025ca59c3ed5a7f931651
Andrej-oss <snowtomsawyer@gmail.com>
2021-09-12 13:40:51 +0200

    - back-end side added

---
+++ src/main/java/com/example/noctuaiordertask/NoctuaiOrderTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.noctuaiordertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoctuaiOrderTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoctuaiOrderTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noctuaiordertask/NoctuaiOrderTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noctuaiordertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoctuaiOrderTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
